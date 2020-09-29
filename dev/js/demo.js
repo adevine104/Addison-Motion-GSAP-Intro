bb079dce9c243e4b50a0c11c05bc52d306f69070
adevine104 <63673128+adevine104@users.noreply.github.com>
2020-09-29 11:09:37 -0400

    reformat

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,52 +1,52 @@
 
 
-import {gsap} from "gsap";
+// import {gsap} from "gsap";
 
 
-var smileySpeed = .8;
+// var smileySpeed = .8;
 
-var tl = gsap.timeline({repeat: -1, repeatDelay: .05});
-
-
-tl.to(".box", {
-    duration: .03,
-    y: 0,
-    scaleX:1
-});
-
-tl.to(".box", {
-    duration: .5,
-    y: -360,
-    scaleX: 1
-});
-
-tl.to(".box", {
-    duration: smileySpeed,
-    y: 0,
-    scaleX:1.2
-});
-
-tl.to(".box", {
-    duration: .5,
-    y: -360,
-    scaleX: 1
-});
-
-tl.to(".box", {
-    duration: smileySpeed,
-    y: 0,
-    scaleX:1.2
-});
-
-tl.to(".box", {
-    duration: .5,
-    y: -360,
-    scaleX: 1
-});
+// var tl = gsap.timeline({repeat: -1, repeatDelay: .05});
 
 
-tl.to(".box", {
-    duration: smileySpeed,
-    y: 0,
-    scaleX:1.2
-});
+// tl.to(".box", {
+//     duration: .03,
+//     y: 0,
+//     scaleX:1
+// });
+
+// tl.to(".box", {
+//     duration: .5,
+//     y: -360,
+//     scaleX: 1
+// });
+
+// tl.to(".box", {
+//     duration: smileySpeed,
+//     y: 0,
+//     scaleX:1.2
+// });
+
+// tl.to(".box", {
+//     duration: .5,
+//     y: -360,
+//     scaleX: 1
+// });
+
+// tl.to(".box", {
+//     duration: smileySpeed,
+//     y: 0,
+//     scaleX:1.2
+// });
+
+// tl.to(".box", {
+//     duration: .5,
+//     y: -360,
+//     scaleX: 1
+// });
+
+
+// tl.to(".box", {
+//     duration: smileySpeed,
+//     y: 0,
+//     scaleX:1.2
+// });
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,3 +1,17 @@
-import * as Demo from './demo.js';
+// import * as Demo from './demo.js';
 
-console.log(Demo);
+// console.log(Demo);
+
+import {gsap} from "gsap";
+
+import {smileyAnimation} from "./smiley-face.js"
+// import {blueBoxAnimation} from "./blueBox.js"
+
+
+const mainTL = gsap.timeline({paused:true});
+
+    mainTL.add(smileyAnimation())
+        //.addLabel("orange")
+        // .add(orangeBoxAnimation())
+        .play();
+
